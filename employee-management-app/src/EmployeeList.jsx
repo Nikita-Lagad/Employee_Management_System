bab0450e5106070d0c83eaeeadb6c585287b08f8
Nikita-Lagad <nikitalagad98@gmail.com>
2024-05-21 20:32:00 +0530

    new features added

--- employee-management-app/src/EmployeeList.jsx
+++ employee-management-app/src/EmployeeList.jsx
@@ -1,9 +1,11 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import './Employee.css'; 
+import axios from 'axios';
+import EmployeeUpdate from './EmployeeUpdate';
 
 const EmployeeList = () => {
     const [employees, setEmployees] = useState([]);
+    const [showUpdateForm, setShowUpdateForm] = useState(false);
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
 
     useEffect(() => {
         const fetchEmployees = async () => {
@@ -23,11 +25,17 @@ const EmployeeList = () => {
         fetchEmployees();
     }, []);
 
+    const handleUpdateClick = (employee) => {
+        setSelectedEmployee(employee);
+        setShowUpdateForm(true);
+    };
+
     return (
         <div className="container">
-            <h1>Employee List</h1>
-            <table>
-                <thead>
+            <h1 className="text-center font-weight-bold my-4">Employee List</h1>
+            {showUpdateForm && <EmployeeUpdate employee={selectedEmployee} />}
+            <table className="table table-dark table-stripped table-bordered">
+                <thead className='thead-light'>
                     <tr>
                         <th>ID</th>
                         <th>Name</th>
@@ -36,26 +44,28 @@ const EmployeeList = () => {
                         <th>Age</th>
                         <th>Gender</th>
                         <th>Department</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        employees.map((employee, index) => (
-                            <tr key={index}>
-                                <td>{employee.id}</td>
-                                <td>{employee.name}</td>
-                                <td>{employee.mobile}</td>
-                                <td>{employee.email}</td>
-                                <td>{employee.age}</td>
-                                <td>{employee.gender}</td>
-                                <td>{employee.department}</td>
-                            </tr>
-                        ))
-                    }
+                    {employees.map((employee, index) => (
+                        <tr key={index}>
+                            <td>{employee.id}</td>
+                            <td>{employee.name}</td>
+                            <td>{employee.mobile}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.age}</td>
+                            <td>{employee.gender}</td>
+                            <td>{employee.department}</td>
+                            <td>
+                                <button className="btn btn-sm btn-light" onClick={() => handleUpdateClick(employee)}>Update</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
     );
-}
+};
 
 export default EmployeeList;
